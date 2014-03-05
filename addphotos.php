1bfae706d2c47505a55710d60fc8ebfb9f1c6206
jeff.carlsonjr@gmail.com <jeff.carlsonjr@gmail.com>
2014-03-05 10:26:01 -0500

    Most Recent
    These files where just not committed and Sync'd

--- addphotos.php
+++ addphotos.php
@@ -2,7 +2,6 @@
 include 'functions/globalClass.php';
 
 
-<<<<<<< HEAD
 define ('MAX_SIZE', "3000");
 
 $errors=0;
@@ -78,68 +77,3 @@ $width,$height);
                               }
                               
                               }
-=======
-$errors=0;
-
-$image =$_FILES["file"]["name"];
-
-$uploadedfile = $_FILES['file']['tmp_name'];
-
-if ($image) 
-    {
-    list($name,$extension) = explode(".", strtolower($image));
-    if (($extension != "jpg") && ($extension != "jpeg") && ($extension != "png") && ($extension != "gif")) 
-        {
-        echo ' Unknown Image extension ';
-        $errors=1;
-        }
-
-        else
-         {
-         $size=filesize($_FILES['file']['tmp_name']);
-
-          if($extension=="jpg" || $extension=="jpeg" )
-              {
-              $uploadedfile = $_FILES['file']['tmp_name'];
-              $src = imagecreatefromjpeg($uploadedfile);
-              }
-              else if($extension == "png")
-                  {
-                  $uploadedfile = $_FILES['file']['tmp_name'];
-                  $src = imagecreatefrompng($uploadedfile);
-                  }
-                  else 
-                      {
-                      $src = imagecreatefromgif($uploadedfile);
-                      }
-                      list($width,$height) = getimagesize($uploadedfile);
-                      $newwidth = 640;
-                      $newheight = ($height/$width)*$newwidth;
-                      $tmp = imagecreatetruecolor($newwidth,$newheight);
-
-                      $newwidth1 = 150;
-                      $newheight1 = ($height/$width)*$newwidth1;
-                      $tmp1 = imagecreatetruecolor($newwidth1,$newheight1);
-
-                      imagecopyresampled($tmp,$src,0,0,0,0,$newwidth,$newheight,$width,$height);
-                      imagecopyresampled($tmp1,$src,0,0,0,0,$newwidth1,$newheight1, $width,$height);
-                      
-
-                      $filename = "images/". $_FILES['file']['name'];
-                      $filename1 = "imageThumbs/". $_FILES['file']['name'];
-                      
-                      imagejpeg($tmp,$filename,100);
-                      imagejpeg($tmp1,$filename1,100);
-                      
-                      $query = "INSERT INTO photos SET path = '".$image."',alt = '".filter_input(INPUT_POST, 'location')."',date = NOW(); ";
-                      mysql_query($query) or die(mysql_error());
-                     
-                      imagedestroy($src);
-                      imagedestroy($tmp);
-                      imagedestroy($tmp1);
-                      
-                       echo "<meta http-equiv='refresh' content='0;url=photos.php' />";
-                      }
-                      }
-                                      
->>>>>>> FETCH_HEAD
--- forms.php
+++ forms.php
@@ -2,17 +2,26 @@
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
+<<<<<<< HEAD
 and open the template in the editor.
 -->
 <html ng-app="APP">
     <head>
         <title>Welcome to Jeff's Forms</title>
+=======
+and open the template in the editor. http://www.ng-newsletter.com/posts/validations.html
+-->
+<html>
+    <head>
+        <title>Welcome to Forms</title>
+>>>>>>> FETCH_HEAD
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="./bootstrap/css/bootstrap.css" rel='stylesheet'/>
         <link href="./css/stylesheet.css" rel="stylesheet" />
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script src="./js/angular.js"></script>
+<<<<<<< HEAD
         <script src="./js/app.js"></script>
         <script src="./js/formCtrl.js"></script>
         <script src="./js/angular-route.js"></script>
@@ -67,6 +76,36 @@ and open the template in the editor.
                             <br><button type="submit" ng-disabled="signup_form.$invalid" class="btn-primary">Submit</button>
                     
                 </form>
+=======
+        <script src="./js/formCtrl.js"></script>
+  
+    </head>
+    <body ng-app>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 forms" >
+                    
+                    <form name="signup_form" class="form-group" ng-submit="signupForm()" novalidate>
+                        <fieldset>
+                            <legend>Sign Form</legend>
+                            <label>First Name</label><br>
+                            <input type="text" placeholder="First Name" name="fName" ng-model="user.fname" ng-minlength=3 ng-maxlength=20 class="form-control" required/>
+                            <div class="error" ng-show="signup_form.fName.$dirty && signup_form.fName.$invalid">
+                                <small class="error" ng-show="signup_form.fName.$error.required">
+                                    Your first name is required.
+                                </small>
+                                <small class="error" ng-show="signup_form.fName.$error.minlength">
+                                        Your name is required to be at least 3 characters
+                                </small>
+                                <small class="error" ng-show="signup_form.fName.$error.maxlength">
+                                        Your name cannot be longer than 20 characters
+                                </small>
+                            </div>
+                            
+                            <br><button type="submit" class="btn-primary">Sign Up</button>
+                        </fieldset>
+                    </form>
+>>>>>>> FETCH_HEAD
                     
                 </div>
             </div>
