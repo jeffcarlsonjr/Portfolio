4620a42b1d51825eb52285c50dc6e9694f198f7b
jeff.carlsonjr@gmail.com <jeff.carlsonjr@gmail.com>
2014-03-07 10:15:04 -0500

    Couple more corrections

--- applications.php
+++ applications.php
@@ -1,10 +1,12 @@
 <?php
 include 'functions/globalClass.php';
 $tools = new tools();
+
+
 if(empty($_SESSION['id']))
 {
     
-    $tools->metaRedirect('0', '/login');
+    $tools->metaRedirect('0', '/Portfolio/#/login');
 }
 else {}
 echo $_SESSION['id'];
